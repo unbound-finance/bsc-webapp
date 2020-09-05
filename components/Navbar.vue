55faa8c90da9f5f7c8756cb50bbf84e1a54d20ef
drcyph3r <sb4514.work@gmail.com>
2020-09-05 11:50:48 +0530

    ft: change wallet

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -33,17 +33,69 @@
         />
       </button>
     </div>
+    <Modal :show="ui.showDialog" @close="ui.showDialog = false">
+      <template>
+        <div class="flex flex-col space-y-4">
+          <div class="flex justify-between items-center">
+            <p class="font-medium dark:text-white">Account</p>
+            <button
+              type="button"
+              class="focus:outline-none"
+              @click="ui.showDialog = false"
+            >
+              <i class="fas fa-times text-gray-900 dark:text-gray-500"></i>
+            </button>
+          </div>
+
+          <div
+            class="rounded-lg border border-gray-300 dark:border-gray-800 p-4"
+          >
+            <div class="flex justify-between items-center">
+              <p class="text-sm text-gray-600">Connected with Metamask</p>
+              <button
+                type="button"
+                class="focus:outline-none px-2 py-1 rounded text-sm bg-light-primary dark:bg-dark-primary bg-opacity-25 text-light-primary dark:text-white"
+              >
+                Change
+              </button>
+            </div>
+
+            <div class="w-full md:w-1/2 pt-4">
+              <p class="truncate font-medium text-md md:text-xl">
+                {{ address }}
+              </p>
+            </div>
+
+            <div class="mt-2">
+              <a
+                :href="`https://etherscan.io/address/${address}`"
+                target="_blank"
+                class="flex items-center space-x-1 hover:underline"
+              >
+                <i class="fas fa-external-link-alt text-gray-600 text-xs"></i>
+                <span class="text-gray-600 text-sm">View on Etherscan</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </template>
+    </Modal>
   </div>
 </template>
 
 <script>
 import { ethers } from 'ethers'
 import Web3 from 'web3'
+import Modal from '@/components/_app/Modal'
 
 export default {
+  components: { Modal },
   data() {
     return {
       address: '',
+      ui: {
+        showDialog: true,
+      },
     }
   },
 
