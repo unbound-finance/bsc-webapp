9926ba2b86f9b64086bca6c8f402ae6d8b994476
drcyph3r <sb4514.work@gmail.com>
2020-09-13 19:39:23 +0530

    ft: awaiting modal

---
+++ components/modals/AwaitingModal.vue
@@ -0,0 +1,45 @@
+<template>
+  <Modal v-model="modal">
+    <div class="flex flex-col justify-center py-4 items-center space-y-4">
+      <img
+        src="~/assets/loader.svg"
+        width="72"
+        class="animateLoader"
+        alt="success"
+      />
+      <div class="pt-4">
+        <p class="text-xl dark:text-white font-medium text-gray-700">
+          Waiting For Confirmation
+        </p>
+        <p class="text-xs text-center text-gray-500 dark:text-gray-700">
+          Confirm this transaction in your wallet.
+        </p>
+      </div>
+    </div>
+  </Modal>
+</template>
+
+<script>
+import Modal from '@/components/_app/Modal'
+export default {
+  props: {
+    hash: String,
+  },
+  extends: Modal,
+}
+</script>
+
+<style>
+.animateLoader {
+  animation: 2s linear 0s infinite normal none rotateIt;
+}
+
+@keyframes rotateIt {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
