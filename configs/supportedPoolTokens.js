e326a99478278bf5a4429282e00a0fb86cb2a706
Chetan Badhe <csbadhe08@gmail.com>
2020-09-22 02:25:14 +0530

    ft: update addresses

--- configs/supportedPoolTokens.js
+++ configs/supportedPoolTokens.js
@@ -3,7 +3,7 @@ export default [
     name: 'UNI-ETH/DAI',
     exchange: 'Uniswap',
     address: '0x266480906Fd0aa3edD7fF64f466ea9684b792179',
-    llcAddress: '0x5BF618876a235601dC20970c4FbEC72586C44802',
+    llcAddress: '0x9d8045B3BaC4B71ea82B12E7d35Dbf3054279e4c',
     currencyOneLogo: 'https://uniswap.info/static/media/eth.73dabb37.png',
     currencyTwoLogo:
       'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x6B175474E89094C44Da98b954EedeAC495271d0F/logo.png',
@@ -14,7 +14,7 @@ export default [
     name: 'UNI-LINK/DAI',
     exchange: 'Uniswap',
     address: '0xe8163ad34c285f7ad9b4ac0374db7fe10c67e169',
-    llcAddress: '0xbCB9da0028cE43dB1Ca36691FDff1e854E4eCCce',
+    llcAddress: '0x5642beA6E3fb85a43931d979287b06851A302121',
     currencyOneLogo: 'https://uniswap.info/static/media/eth.73dabb37.png',
     currencyTwoLogo:
       'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x514910771AF9Ca656af840dff83E8264EcF986CA/logo.png',
