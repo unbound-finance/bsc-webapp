f830e691d35e57fd149f9707158767cd2bf8c5d6
drcyph3r <sb4514.work@gmail.com>
2020-10-05 11:36:10 +0530

    ft: calculator ui

---
+++ configs/pairs.js
@@ -0,0 +1,31 @@
+export default [
+  {
+    name: 'USDC-ETH',
+    exchange: 'Uniswap',
+    address: '0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc',
+    currencyOneLogo:
+      'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png',
+    currencyTwoLogo: 'https://uniswap.info/static/media/eth.73dabb37.png',
+
+    stablecoin: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+  },
+  {
+    name: 'USDT-ETH',
+    exchange: 'Uniswap',
+    address: '0x0d4a11d5eeaac28ec3f61d100daf4d40471f1852',
+    currencyOneLogo:
+      'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xdAC17F958D2ee523a2206206994597C13D831ec7/logo.png',
+    currencyTwoLogo: 'https://uniswap.info/static/media/eth.73dabb37.png',
+
+    stablecoin: '0xdac17f958d2ee523a2206206994597c13d831ec7',
+  },
+  {
+    name: 'DAI-ETH',
+    exchange: 'Uniswap',
+    address: '0xa478c2975ab1ea89e8196811f51a7b7ade33eb11',
+    currencyOneLogo:
+      'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x6B175474E89094C44Da98b954EedeAC495271d0F/logo.png',
+    currencyTwoLogo: 'https://uniswap.info/static/media/eth.73dabb37.png',
+    stablecoin: '0x6b175474e89094c44da98b954eedeac495271d0f',
+  },
+]
--- pages/calculator.vue
+++ pages/calculator.vue
@@ -1,39 +1,117 @@
 <template>
   <div>
     <form @submit.prevent="calculate">
-      <input
-        v-model="calcData.pair"
-        class="appearance-none block w-full dark:bg-dark-bg text-gray-700 dark:text-gray-500 border border-gray-200 dark:border-gray-700 rounded py-3 px-4 leading-tight focus:outline-none focus:border-gray-500"
-        type="text"
-        placeholder="Pair Address"
-      />
-      <input
-        v-model="calcData.stablecoinAddress"
-        class="appearance-none mt-4 block w-full dark:bg-dark-bg text-gray-700 dark:text-gray-500 border border-gray-200 dark:border-gray-700 rounded py-3 px-4 leading-tight focus:outline-none focus:border-gray-500"
-        type="text"
-        placeholder="Stablecoin Address"
-      />
-      <div class="flex items-center space-x-4 mt-4">
+      <div class="space-y-1">
+        <label class="block text-sm leading-5 font-medium text-gray-700 p-1">
+          Select Pair
+        </label>
+        <div class="relative">
+          <span class="inline-block w-full rounded-md">
+            <button
+              type="button"
+              aria-haspopup="listbox"
+              aria-expanded="true"
+              aria-labelledby="listbox-label"
+              class="cursor-default relative w-full rounded-md border border-gray-300 dark:border-gray-700 bg-white dark:text-white dark:bg-dark-bg pl-3 pr-10 py-2 text-left focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+              @click="ui.dropModal = !ui.dropModal"
+            >
+              <div class="flex items-center space-x-3">
+                <double-logo
+                  :token0logo="selectedPair.currencyOneLogo"
+                  :token1logo="selectedPair.currencyTwoLogo"
+                />
+                <span class="block truncate"> {{ selectedPair.name }} </span>
+              </div>
+              <span
+                class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none"
+              >
+                <svg
+                  class="h-5 w-5 text-gray-400"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path
+                    d="M7 7l3-3 3 3m0 6l-3 3-3-3"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </span>
+            </button>
+          </span>
+
+          <div
+            v-if="ui.dropModal"
+            class="absolute mt-1 w-full rounded-md bg-white dark:bg-dark-bg dark:text-white shadow-lg"
+          >
+            <ul
+              tabindex="-1"
+              role="listbox"
+              aria-labelledby="listbox-label"
+              aria-activedescendant="listbox-item-3"
+              class="max-h-56 rounded-md py-1 text-base leading-6 shadow-xs overflow-auto focus:outline-none sm:text-sm sm:leading-5"
+            >
+              <li
+                v-for="pair in pairs"
+                id="listbox-item-0"
+                :key="pair.address"
+                role="option"
+                class="text-gray-900 cursor-default select-none relative py-2 pl-3 pr-9 hover:bg-accent hover:text-white"
+                @click="selectPair(pair)"
+              >
+                <div class="flex items-center space-x-3">
+                  <double-logo
+                    :token0logo="pair.currencyOneLogo"
+                    :token1logo="pair.currencyTwoLogo"
+                  />
+                  <span class="font-normal block truncate dark:text-white"
+                    >{{ pair.name }}
+                  </span>
+                </div>
+              </li>
+
+              <!-- More options... -->
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="mt-4">
+        <label class="block text-sm leading-5 font-medium text-gray-700 p-1">
+          LTV
+        </label>
         <input
           v-model="calcData.ltv"
           class="appearance-none block w-full dark:bg-dark-bg text-gray-700 dark:text-gray-500 border border-gray-200 dark:border-gray-700 rounded py-3 px-4 leading-tight focus:outline-none focus:border-gray-500"
           type="text"
-          placeholder="LTV"
+          placeholder="30"
         />
       </div>
       <div class="flex items-center space-x-4 mt-4">
-        <input
-          v-model="calcData.lossPercentage"
-          class="appearance-none block w-full dark:bg-dark-bg text-gray-700 dark:text-gray-500 border border-gray-200 dark:border-gray-700 rounded py-3 px-4 leading-tight focus:outline-none focus:border-gray-500"
-          type="text"
-          placeholder="Price Change (%)"
-        />
-        <input
-          v-model="calcData.days"
-          class="appearance-none block w-full dark:bg-dark-bg text-gray-700 dark:text-gray-500 border border-gray-200 dark:border-gray-700 rounded py-3 px-4 leading-tight focus:outline-none focus:border-gray-500"
-          type="text"
-          placeholder="Days"
-        />
+        <div>
+          <label class="block text-sm leading-5 font-medium text-gray-700 p-1">
+            Price Change (%)
+          </label>
+          <input
+            v-model="calcData.lossPercentage"
+            class="appearance-none block w-full dark:bg-dark-bg text-gray-700 dark:text-gray-500 border border-gray-200 dark:border-gray-700 rounded py-3 px-4 leading-tight focus:outline-none focus:border-gray-500"
+            type="text"
+            placeholder="-50"
+          />
+        </div>
+
+        <div>
+          <label class="block text-sm leading-5 font-medium text-gray-700 p-1">
+            Duration (days)
+          </label>
+          <input
+            v-model="calcData.days"
+            class="appearance-none block w-full dark:bg-dark-bg text-gray-700 dark:text-gray-500 border border-gray-200 dark:border-gray-700 rounded py-3 px-4 leading-tight focus:outline-none focus:border-gray-500"
+            type="text"
+            placeholder="30"
+          />
+        </div>
       </div>
       <button
         type="submit"
@@ -74,27 +152,49 @@
 
 <script>
 import axios from 'axios'
+
+import pairs from '../configs/pairs'
 export default {
   data() {
     return {
+      ui: {
+        dropModal: false,
+      },
+      pairs,
+      selectedPair: {
+        name: 'USDC-ETH',
+        exchange: 'Uniswap',
+        address: '0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc',
+        currencyOneLogo:
+          'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png',
+        currencyTwoLogo: 'https://uniswap.info/static/media/eth.73dabb37.png',
+
+        stablecoin: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+      },
       calcData: {
-        pair: '',
-        stablecoinAmt: '',
         lossPercentage: '',
         days: '',
         ltv: '',
-        stablecoinAddress: '',
       },
       calcResult: null,
     }
   },
   methods: {
+    selectPair(pair) {
+      this.selectedPair = pair
+      this.ui.dropModal = false
+    },
     async calculate() {
       try {
         const result = await axios({
           method: 'post',
           url: 'https://unbound-liquidation-check.vercel.app/api/getNetValue',
-          data: this.calcData,
+          data: {
+            pair: this.selectedPair.address,
+            stablecoinAddress: this.selectedPair.stablecoin,
+            loanPercentage: this.calcData.ltv,
+            days: this.calcData.days,
+          },
           headers: {
             'Content-Type': 'application/json',
           },
