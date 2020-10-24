3cd05d67b578787ce556e544515156f26622f3aa
drcyph3r <sb4514.work@gmail.com>
2020-10-24 19:05:33 +0530

    ft: Abbreviate Number

--- plugins/numberFormatter.js
+++ plugins/numberFormatter.js
@@ -1,54 +1,20 @@
-// known SI prefixes, multiple of 3
-// const PREFIXES = {
-//   12: 'T',
-//   9: 'B',
-//   6: 'M',
-//   3: 'K',
-//   0: '',
-// }
-
-// function getExponent(n) {
-//   if (n === 0) return 0
-
-//   return Math.floor(Math.log10(Math.abs(n)))
-// }
-
-// function precise(n) {
-//   return Number.parseFloat(n.toPrecision(4))
-// }
-
-// function toHumanString(sn) {
-//   const n = precise(Number.parseFloat(sn))
-//   let e = Math.min(Math.floor(getExponent(n)), 6)
-
-//   if (e % 2 === 0) e -= 1
-//   if (e < 2) e = 0
-
-//   return precise(n / Math.pow(10, e)).toString() + PREFIXES[e]
-// }
-
-function abbreviateNumber(value) {
-  let newValue = value
-  if (value >= 1000) {
-    const suffixes = ['', 'k', 'm', 'b', 't']
-    const suffixNum = Math.floor(('' + value).length / 3)
-    let shortValue = ''
-    for (let precision = 2; precision >= 1; precision--) {
-      shortValue = parseFloat(
-        (suffixNum !== 0
-          ? value / Math.pow(1000, suffixNum)
-          : value
-        ).toPrecision(precision)
-      )
-      const dotLessShortValue = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g, '')
-      if (dotLessShortValue.length <= 2) {
-        break
-      }
-    }
-    if (shortValue % 1 !== 0) shortValue = shortValue.toFixed(1)
-    newValue = shortValue + suffixes[suffixNum]
-  }
-  return newValue
+function abbreviateNumber(num, fixed) {
+  if (num === null) {
+    return null
+  } // terminate early
+  if (num === 0) {
+    return '0'
+  } // terminate early
+  fixed = !fixed || fixed < 0 ? 0 : fixed // number of decimal places to show
+  const b = num.toPrecision(2).split('e') // get power
+  const k = b.length === 1 ? 0 : Math.floor(Math.min(b[1].slice(1), 14) / 3) // floor at decimals, ceiling at trillions
+  const c =
+    k < 1
+      ? num.toFixed(0 + fixed)
+      : (num / Math.pow(10, k * 3)).toFixed(1 + fixed) // divide by power
+  const d = c < 0 ? c : Math.abs(c) // enforce -0 is 0
+  const e = d + ['', 'K', 'M', 'B', 'T'][k] // append power
+  return e
 }
 
 export default (_, inject) => {
