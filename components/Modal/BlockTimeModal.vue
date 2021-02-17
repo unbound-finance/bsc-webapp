0de8fbce0773de86dcccd9d86ac154cced1fb6cb
Saurabh Bachawe <sb4514.work@gmail.com>
2021-02-17 19:47:57 +0530

    ft: block limit modal

--- components/Modal/BlockTimeModal.vue
+++ components/Modal/BlockTimeModal.vue
@@ -1,8 +1,13 @@
 <template>
   <Modal v-model="modal">
     <div class="flex flex-col justify-center px-4 py-8 items-center space-y-4">
-      <div>
-        <p class="text--600 font-medium">Cool Down ❄️</p>
+      <div class="text-center">
+        <p class="text-lg text-gray-800 dark:text-white font-medium">
+          Cool Down ❄️
+        </p>
+        <p class="text-xs text-gray-600 dark:text-gray-700">
+          Ruko jara, sabar karo 🖐
+        </p>
         <button
           class="w-full px-4 py-2 rounded mt-4 bg-blue-600 bg-opacity-25 text-blue-600 font-medium focus:outline-none"
           @click="modal = false"
