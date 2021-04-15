bd727055f8b835c90b677510b4bb663768f0712c
Saurabh Bachawe <sb4514.work@gmail.com>
2021-04-15 10:44:31 +0530

    ft: bscscan link

--- components/Modal/SuccessModal.vue
+++ components/Modal/SuccessModal.vue
@@ -6,8 +6,8 @@
         <p class="text-xl dark:text-white font-medium text-gray-800">
           Transaction Submitted
         </p>
-        <a :href="`https://kovan.etherscan.io/tx/${hash}`" target="_blank">
-          <p class="text-xs text-center text-accent">View on Etherscan</p>
+        <a :href="`https://testnet.bscscan.io/tx/${hash}`" target="_blank">
+          <p class="text-xs text-center text-accent">View on Bscscan</p>
         </a>
         <button
           class="w-full px-4 py-2 rounded mt-4 bg-dark-primary bg-opacity-25 text-dark-primary font-medium focus:outline-none"
